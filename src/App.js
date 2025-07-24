532000ec63a9f2fb8a760ca9d95d4dae01df0f98
HammadGH123 <hammadali065@gmail.com>
2025-07-24 16:28:39 +0530

    Update site content

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <HashRouter>
       <Topbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio-react" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
